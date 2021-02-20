e1264852a3be7b72ccebee8aaa1141f45ebcdcdd
Alexey <ogobel@icloud.com>
2021-02-20 19:29:29 +0300

    feat: add dreameam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,5 +1,33 @@
 const CustomError = require("../extensions/custom-error");
+function isString(val) {
+  return (typeof val === "string" || val instanceof String);
+}
+module.exports = function createDreamTeam(members) {
 
-module.exports = function createDreamTeam(sampleActivity) {
+  if( Array.isArray(members)==false)
+  {
+    return false;
+  }
+    let res =[],
+       str = '';
+      for(let i = 0; i<members.length; i++){
+           str = members[i];
+         if( isString(str) == false){
+         continue;
+         }
+         else{
+           let j = 0;
+           while(str[j] == ' '){
+             j++;
+           }
+          res.push(str[j].toUpperCase());
+         }
+    }
+    res.sort(function(a, b){
+      if(a < b) { return -1; }
+  if(a > b) { return 1; }
+  return 0;
+    });
+    return res.join('');
+    }
 
-};
