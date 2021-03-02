214869a4d9d20cf9eb22b6524c49180a2447348d
ogobelbel <74075526+ogobelbel@users.noreply.github.com>
2021-03-02 19:00:01 +0300

    fix: add some conditions

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,12 +4,11 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if(typeof sampleActivity !== "string" || sampleActivity < 0) {
+  if(typeof sampleActivity !== "string" ||!Number(sampleActivity) || Number(sampleActivity) < 0 ||
+    MODERN_ACTIVITY<Number(sampleActivity) || Number(sampleActivity)==0) {
     return false;
   }
- if(!Number(sampleActivity)){
-  return false;
-}
+
 
   let res = Math.log(MODERN_ACTIVITY/Number(sampleActivity))/(0.693/HALF_LIFE_PERIOD);
   return Math.ceil(res);
