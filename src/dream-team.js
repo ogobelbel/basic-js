c64bf10e94bad6922ac88dc1377dfb0300b7955e
ogobelbel <74075526+ogobelbel@users.noreply.github.com>
2021-03-02 14:51:36 +0300

    fix: del

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,33 +1,6 @@
 const CustomError = require("../extensions/custom-error");
-function isString(val) {
-  return (typeof val === "string" || val instanceof String);
-}
-module.exports = function createDreamTeam(members) {
 
-  if( Array.isArray(members)==false)
-  {
-    return false;
-  }
-    let res =[],
-       str = '';
-      for(let i = 0; i<members.length; i++){
-           str = members[i];
-         if( isString(str) == false){
-         continue;
-         }
-         else{
-           let j = 0;
-           while(str[j] == ' '){
-             j++;
-           }
-          res.push(str[j].toUpperCase());
-         }
-    }
-    res.sort(function(a, b){
-      if(a < b) { return -1; }
-  if(a > b) { return 1; }
-  return 0;
-    });
-    return res.join('');
-    }
-
+module.exports = function createDreamTeam(/* members */) {
+  throw new CustomError('Not implemented');
+  // remove line with error and write your code here
+};
