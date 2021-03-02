bfbf4c940f769a874ea11ed10c6b2e5e78c1e635
ogobelbel <74075526+ogobelbel@users.noreply.github.com>
2021-03-02 16:48:02 +0300

    fix: del if

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -10,10 +10,7 @@ module.exports = function dateSample(sampleActivity) {
     sampleActivity < 0) {
     return false;
   }
-  const activity = parseFloat(sampleActivity);
-  if(!activity) {
-    return false;
-  }
+
   let res = Math.log(MODERN_ACTIVITY/Number(sampleActivity))/(0.693/HALF_LIFE_PERIOD);
   return Math.ceil(res);
 };
