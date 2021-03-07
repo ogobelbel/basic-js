89c178b35a366eb79b5d9ca5bb443eb9fd0f86db
ogobelbel <74075526+ogobelbel@users.noreply.github.com>
2021-03-07 23:19:33 +0300

    fix: add some bugs and fiches

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,50 +1,53 @@
-const CustomError = require("../extensions/custom-error");
 
 
 
-let count = 1;
+let count = 0;
 let count1 = 0;
 let flag = true;
 let mass = [];
-let i = 0;
+let a = 0;
 let arrNumber = 0;
 module.exports = class DepthCalculator {
   calculateDepth(arr) {
     {
       for (let i = 0; i < arr.length; i++) { //удаление цифр из массива
-        if(Array.isArray(arr[i])==false){
+          if(Array.isArray(arr[i])==false){
           arr.splice(i, 1);
           i--;
+          }
         }
-      }
        // сохранение изначального массива
       if(flag==true){
          mass = arr.slice();
         flag=false;
       }
-      if((arr[0] )){
-        count++;
+     
+      if(arr[0]){ 
+     // углубление
+     count++;
+        return this.calculateDepth(arr[0]);
         
       }
-      if(arr[0]){  // углубление
-        return this.calculateDepth( arr[0]);
-      }
-      
       else{ // если массив пустой переходим к некст элементу
         arrNumber++;
         if(count>=count1){
-         
           count1=count;
         }
-        count = 1;
-      
-        if(mass[arrNumber]){ // если есть элемент в изначальном массиве - идем к нему
+        count = 0;
+        if(mass[arrNumber]){
+          count++; // если есть элемент в изначальном массиве - идем к нему
           return this.calculateDepth(mass[arrNumber]);
         }
-         // если нет - возвращаем макс глубину
-          return +count1; 
-        
+        else {
+          a = count1+1;
+           count = 0;
+   count1 = 0;
+ flag = true;
+ mass = [];
+ arrNumber = 0;
+           // если нет - возвращаем макс глубину
+          return a;
+        }
       }
     }
-  }
-}
+  }}
