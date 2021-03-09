11669feeae0983bc89e38a48fd18020523fe5668
ogobelbel <74075526+ogobelbel@users.noreply.github.com>
2021-03-09 14:51:46 +0300

    fix: add mod

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,3 +3,4 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function repeater(/* str, options */) {
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
+module.exports = VigenereCipheringMachine;
