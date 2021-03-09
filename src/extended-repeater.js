3fcaf512433af4445a1a2050d089479c0adc5b77
ogobelbel <74075526+ogobelbel@users.noreply.github.com>
2021-03-09 15:12:03 +0300

    fix: add check null

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,6 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options){
+  if (str != null) {
+    str = str.toString();
+}
+  if(!options.additionRepeatTimes){
+    options.additionRepeatTimes = 1;
+  }
+  if(!options.repeatTimes){
+    options.repeatTimes = 1;
+  }
+  if(!options.separator){
+    options.separator = '+';
+  }
+ 
+    let strRes = '';
+  console.log(options.repeatTimes);
+    for (let i = 0; i < options.repeatTimes; i++) {
+        strRes+=str;
+        if((options.addition||options.addition===false||options.addition===null)){
+            for (let j = 0; j < options.additionRepeatTimes; j++) {
+                strRes+=options.addition;
+                if(options.additionRepeatTimes>1 && j != options.additionRepeatTimes-1){
+                    strRes+=options.additionSeparator;
+                }
+            }
+        }
+        if(options.repeatTimes>1 && i != options.repeatTimes-1){
+            strRes+=options.separator;
+        }
+    }
+ 
+   return strRes;
 };
+  
+
